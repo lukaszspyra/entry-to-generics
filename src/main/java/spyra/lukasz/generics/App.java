1e68ba070c0c92904df708af553d217dbc882fc7
lukaszspyra <lukasz.spyrka@gmail.com>
2020-12-17 23:16:36 +0100

    Create empty project

---
+++ src/main/java/spyra/lukasz/generics/App.java
@@ -0,0 +1,13 @@
+package spyra.lukasz.generics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
